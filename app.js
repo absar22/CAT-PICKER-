0b3fb81fab7fd03601d71a76e61d111d4760b033
Absar Ahmad <absarahmad137@gmail.com>
2025-07-24 22:56:48 +0530

    Added async await features

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-document.querySelector('button').addEventListener('click', getImage)
+document.querySelector('button').addEventListener('click', getACuteCatPhoto)
 
-function getImage(){
-fetch('https://api.thecatapi.com/v1/images/search')
-.then(res => res.json())
-.then(data => {
-  document.querySelector('img').src = data[0].url
-  // console.log(data[0].url)
-})
+async function getACuteCatPhoto() {
+  try {
+    const img = document.querySelector('img')
+    const status = document.querySelector('#status')
 
-.catch(err => {
-  console.log(`Error ${err}`)
-})
+    status.textContent = 'Loading a super cute cat... 😻'
+    img.src = '' // Clear previous image
 
+    const res = await fetch('https://api.thecatapi.com/v1/images/search')
+    const data = await res.json()
+
+    img.src = data[0].url
+    img.style.width = '500px'
+
+    status.textContent = 'Here you go! 🐾'
+  } catch (err) {
+    document.querySelector('#status').textContent = 'Oops! Something went wrong. 😿'
+    console.log(`Error: ${err}`)
+  }
 }
+
